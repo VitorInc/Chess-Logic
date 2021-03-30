c27169ecf6c3b43424c1e897d32168ec93e0e1f6
VitorInc <vitordc.contato@gmail.com>
2021-03-29 23:57:27 -0300

    Start to implement Board and Piece

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,3 +1,14 @@
 package Application;
+
+import boardGame.Board;
+import boardGame.Position;
+
+import java.awt.*;
+
 public class Program {
+    public static void main(String[] args) {
+
+        Board tabuleiro = new Board(8, 8);
+
+    }
 }
