c21d7123d24094b83fe99792f78f07aa84c56f50
VitorInc <vitordc.contato@gmail.com>
2021-04-01 23:56:06 -0300

    Place pieces on the board

--- src/Application/UI.java
+++ src/Application/UI.java
@@ -18,7 +18,7 @@ public class UI {
         else{
             System.out.println(piece);
         }
-        System.out.println(" ");
+        System.out.println("");
 
 
         }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,12 +1,17 @@
 package chess;
 
 import boardGame.Board;
+import boardGame.Position;
+import chess.pieces.Rook;
+
+import java.awt.*;
 
 public class ChessMatch {
     private Board board;
 
     public ChessMatch() {
         board = new Board(8, 8);
+        initilSetup();
     }
     public ChessPiece[][] getPieces(){
         ChessPiece [][] mat = new ChessPiece[board.getRows()][board.getColumns()];
@@ -18,6 +23,11 @@ public class ChessMatch {
 
         }
         return mat;
+    }
+
+    private void initilSetup() {
+        board.placePiece(new Rook(board, Color.BLACK), new  Position( 2, 1));
+        board.placePiece(new King(board, Color.BLACK), new  Position( 0, 4));
     }
 
 }
