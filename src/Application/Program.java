fdf51f1dfabba079161931c04e97063c009a64ef
VitorInc <vitordc.contato@gmail.com>
2021-04-07 16:54:38 -0300

    Add all pieces, also handling exceptions and claring screen

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -3,6 +3,7 @@ package Application;
 
 import boardGame.Board;
 import boardGame.Position;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -16,16 +17,23 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
 
         while(true) {
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("source: ");
-            ChessPosition source =UI.readChessPosition(in);
+            try{
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("source: ");
+                ChessPosition source =UI.readChessPosition(in);
 
-            System.out.println();
-            System.out.print("target: ");
-            ChessPosition target =UI.readChessPosition(in);
+                System.out.println();
+                System.out.print("target: ");
+                ChessPosition target =UI.readChessPosition(in);
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source,target);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source,target);
+            }
+            catch (ChessException e){
+                System.out.println(e.getMessage());
+                in.nextLine();
+            }
+
         }
     }
 }
